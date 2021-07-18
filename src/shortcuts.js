15a1561828ad7684a8d956fb2a7d73cd19a2587b
KM <the.real.laplace@gmail.com>
2021-07-18 17:04:04 +0300

    chore(commit): force

--- src/shortcuts.js
+++ src/shortcuts.js
@@ -1,17 +1,29 @@
+const sdl = require('@kmamal/sdl')
 
 const allModifiers = [ 'ctrl', 'shift', 'alt', 'super', 'altgr' ]
 
 const modifiersPattern = `^(?<modifiers>(?:${allModifiers.join('|')})\\+)*`
 const modifiersRegex = new RegExp(modifiersPattern, 'u')
 
-const fromShortcut = (_pattern) => {
+const fromShortcut = (shortcut) => {
 	const pattern = {}
-	const { groups: { modifiers = '' } } = modifiersRegex.exec(_pattern)
+
+	const { groups: { modifiers = '' } } = modifiersRegex.exec(shortcut)
 	const setModifiers = new Set(modifiers.split('+').slice(0, -1))
 	for (const modifier of allModifiers) {
 		pattern[modifier] = setModifiers.has(modifier)
 	}
-	pattern.key = _pattern.slice(modifiers.length)
+
+	const key = shortcut.slice(modifiers.length)
+	if (sdl.keyboard.getScancode(key)) {
+		pattern.key = key
+	} else {
+		if (key.length > 1) {
+			throw Object.assign(new Error("invalid key"), { shortcut, key })
+		}
+		pattern.text = key
+	}
+
 	return pattern
 }
 
